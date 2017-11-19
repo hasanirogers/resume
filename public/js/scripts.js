f071e9d94620a0451c8d47aa3210547b0917e464
zerodivide85 <hasani.rogers@afrotechnologies.com>
2017-11-19 09:49:40 -0500

    field validation progress

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,36 @@
-var domain = window.location.href;
-    submitBtn = document.getElementsByTagName('paper-button')[0];
+function validateForm() {
+  let fields = document.querySelectorAll('[required]');
 
-submitBtn.addEventListener('click', function(event) {
+  // convert the fields nodeList to an array and iterate over each
+  Array.from(fields).forEach((element, index) => {
+    element.addEventListener('blur', function() {
+      //console.log(this.value);
+      isValid = validateElement(element);
+    });
+  });
+}
+
+function validateElement(elm) {
+  if(typeof elm.value != 'undefined') {
+    if (elm.value.length > 2) {
+      return true;
+    }
+  } else {
+    console.log(elm);
+    elm.classList.add('wuuuut');
+    return false;
+  }
+}
+
+function handleContact() {
   event.preventDefault();
 
   let request,
-      formData = new String;
-      form = document.querySelector('form'),
-      submitBtn = document.querySelector('paper-button'),
-      submitBtnMsg = form.querySelector('span');
-      submitBtnMsgText = submitBtnMsg.innerText;
+    formData = new String;
+  form = document.querySelector('form'),
+    submitBtn = document.querySelector('paper-button'),
+    submitBtnMsg = form.querySelector('span');
+  submitBtnMsgText = submitBtnMsg.innerText;
 
   submitBtn.setAttribute('disabled', true);
 
@@ -61,13 +82,22 @@ submitBtn.addEventListener('click', function(event) {
     form.classList.add('contact--error');
     submitBtnMsg.innerHTML = "Oh boy. Something happen and I didn't your message.";
 
-    setTimeout(function() {
+    setTimeout(function () {
       form.classList.remove('contact--error');
       submitBtn.removeAttribute('disabled');
       submitBtnMsg.innerHTML = submitBtnMsgText;
     }, 3000);
   }
 
-  // if validation passes
-  makeRequest();
-});
+  if (isValid) makeRequest();
+}
+
+
+window.onload = function() {
+  let domain = window.location.href,
+    isValid = false,
+    submitBtn = document.getElementsByTagName('paper-button')[0];
+
+  validateForm();
+  submitBtn.addEventListener('click', handleContact);
+}
